e7e8f83a7ed02aec8601615c9019071373a8d43e
clement <clement@debian.2.0.10.rev.sfr.net>
2020-01-30 17:20:37 +0100

    CRUD + Authentification User

---
+++ node_api/api/Models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  role: {
+    type: String,
+    required: true
+  },
+  session_id: {
+    type: String
+  },
+  created_at: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+mongoose.model('User', userSchema);
+
+module.exports = mongoose.model('User');
--- node_api/app.js
+++ node_api/app.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
 const app = express();
 
+mongoose.connect('mongodb://mongo/apinodeippsi');
 
-app.get('/', (req, res) => {
-    res.end("Node server Working !!!")
-});
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+const userRoute = require('./api/Routes/userRoute');
+
+userRoute(app);
 
 app.listen(3000, '0.0.0.0');
