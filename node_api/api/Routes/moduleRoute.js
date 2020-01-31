428be64a1389e3497a12e19f8ed23b63cdda6e27
SelmaniZayd <selmani.zayd@gmail.com>
2020-01-31 11:57:33 +0100

    commit du merge de feature/MiddlewareModule dans develop

--- node_api/api/Routes/moduleRoute.js
+++ node_api/api/Routes/moduleRoute.js
@@ -3,12 +3,14 @@ const moduleMiddleware = require('../middleware/moduleMiddleware');
 
 module.exports = (app) => {
     app.route('/sessions/:session_id/modules')
-    .get(moduleMiddleware.verify_session ,moduleController.list_all_modules)
-    .post(moduleMiddleware.verify_session ,moduleController.create_a_module);
+    .all(moduleMiddleware.verify_session)
+    .get(moduleController.list_all_modules)
+    .post(moduleController.create_a_module);
  
 
     app.route('/sessions/:session_id/modules/:id')
-    .get(moduleMiddleware.verify_session ,moduleController.get_a_module)
-    .delete(moduleMiddleware.verify_session ,moduleController.delete_a_module)
-    .put(moduleMiddleware.verify_session ,moduleController.update_a_module);
+    .all(moduleMiddleware.verify_session)
+    .get(moduleController.get_a_module)
+    .delete(moduleController.delete_a_module)
+    .put(moduleController.update_a_module);
 }
